6af2237cab2905fd4975531b649bffd1c609ad75
JOAO TARRAN <joaotarran@gmail.com>
2023-12-07 16:54:44 -0300

    Product Detail

--- src/components/contentful/CTFL_Img_Loader.js
+++ src/components/contentful/CTFL_Img_Loader.js
@@ -1,20 +1,22 @@
 import Image from "next/image";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { motion as m } from "framer-motion";
+import { basicFade } from "@/lib/animations";
 
 const imageLoader = ({ src, width, quality }) => {
     return `${src}?w=${width}&q=${quality || 75}`;
 };
 
 export default function CTFL_Img_Loader(props) {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleImageLoad = () => {
+        setIsLoading(false);
+    };
+
     return (
-        <Suspense
-            fallback={
-                <div className="CLTF_Image_Loader">
-                    <p>Carregando Imagem...</p>
-                </div>
-            }
-        >
-            <Image className="CLTF_Image" alt={props.alt} loader={imageLoader} {...props} />
+        <Suspense fallback={null}>
+            <Image className={`CLTF_Image ${isLoading ? "" : "fade-in"}`} alt={props.alt} loader={imageLoader} onLoad={handleImageLoad} {...props} />
         </Suspense>
     );
 }
--- src/components/shop/Product_Detail.tsx
+++ src/components/shop/Product_Detail.tsx
@@ -9,32 +9,40 @@ import CTFL_RichText from "@/components/contentful/CTFL_RichText";
 import { useState } from "react";
 
 const Product_Detail = ({ item }: { item: RoupaFloar }) => {
-    const [imageFocus, setImageFocus] = useState(true);
+    const [imageFocus, setImageFocus] = useState(false);
 
     const handleImageFocus = () => {
         setImageFocus(!imageFocus);
     };
 
     return (
-        <m.div variants={basicFade} initial="hidden" animate="visible" exit="hidden" key={item.fields.slug} className="Product_Card">
-            {imageFocus && (
+        <m.div variants={basicFade} initial="hidden" animate="visible" exit="hidden" key={item.fields.slug} className="Product_Card Product_Detail">
+            <div className="Product_Detail_Image_Container">
+                {item.fields.images.map((image, index) => {
+                    return (
+                        <CTFL_Img_Loader
+                            src={image.fields.file.url}
+                            key={image.fields.file.url + index}
+                            alt={image.fields.description}
+                            width={1600}
+                            height={800}
+                            onClick={handleImageFocus}
+                        />
+                    );
+                })}
+            </div>
+
+            {!imageFocus && (
                 <>
-                    <h2 className="Product_Card_Title">{item.fields.name}</h2>
-                    <div className="Product_Card_Content">
-                        <p className="Product_Card_Intro Desktop_Only">{item.fields.intro}</p>
+                    <h2 className="Product_Card_Title Title">{item.fields.name}</h2>
+                    <div className="Product_Card_Content Content">
+                        <CTFL_RichText document={item.fields.description} />
                     </div>
+                    <p className="Dark_Pill">Feita sob medida</p>
                 </>
             )}
 
-            <button className="Btn Product_Card_Visibility_Btn" onClick={handleImageFocus}>
-                {imageFocus ? (
-                    <span className="material-symbols-outlined">visibility_off</span>
-                ) : (
-                    <span className="material-symbols-outlined">visibility</span>
-                )}
-            </button>
-
-            {imageFocus && (
+            {!imageFocus && (
                 <>
                     <m.h3 variants={basicFade} initial="hidden" animate="visible" exit="hidden" className="Product_Card_Price">
                         R$<span className="priceTag">{item.fields.price},00</span>
@@ -48,23 +56,16 @@ const Product_Detail = ({ item }: { item: RoupaFloar }) => {
                     </m.div>
                 </>
             )}
-            <div className="Product_Card_Image_Container">
-                {imageFocus && (
-                    <m.div
-                        variants={basicFade}
-                        initial="hidden"
-                        animate="visible"
-                        exit="hidden"
-                        className="Image_Container_Gradient Top_Gradient"
-                    ></m.div>
-                )}
-                <CTFL_Img_Loader
-                    src={item.fields.images[0].fields.file.url}
-                    alt={item.fields.images[0].fields.description}
-                    width={1600}
-                    height={800}
-                />
-            </div>
+
+            {imageFocus && (
+                <button className="Btn Product_Card_Visibility_Btn" onClick={handleImageFocus}>
+                    {imageFocus ? (
+                        <span className="material-symbols-outlined">visibility_off</span>
+                    ) : (
+                        <span className="material-symbols-outlined">visibility</span>
+                    )}
+                </button>
+            )}
         </m.div>
     );
 };
